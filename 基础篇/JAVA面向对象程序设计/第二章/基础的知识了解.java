06895c2f9c7f9dcdb17a407dceeb30c27c18ece5
duasong111 <98256947+duasong111@users.noreply.github.com>
2023-03-21 22:37:21 +0800

    Create 基础的知识了解.java

---
+++ 基础篇/JAVA面向对象程序设计/第二章/基础的知识了解.java
@@ -0,0 +1,12 @@
+//标识符号  一般C语言有的在这也同样的适用
+package duasong_learning;
+
+public class hello {
+    public static  void main(String [] args){
+        int Pro_score,eng_score,math_score;
+        Pro_score=23;
+        eng_score=45;
+        math_score=90;
+        System.out.println("平均分是："+(Pro_score+eng_score+math_score)/3);
+    }
+}
